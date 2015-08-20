523b70eae009be42dd419d0aa52a4c25e4bfe4a8
Mark Repka <mark@repkam09.com>
2015-08-20 15:46:49 -0400

    Use repkam09.com rest api as a cors proxy

--- src/util/utility.js
+++ src/util/utility.js
@@ -2,14 +2,28 @@ var reddit = require('../testjson');
 
 module.exports = {
 
-	verbose: function (message) {
-		console.log(message);
-	},
-	
-	getPage : function (url) {
-		return new Promise(function (resolve, reject) {
-			resolve(reddit.nexus4);
-		});
-	}
-	
+    verbose: function (message) {
+        console.log(message);
+    },
+
+    getPage: function (url) {
+        return new Promise(function (resolve, reject) {
+            var url = 'https://www.reddit.com/r/aww/';
+
+            $.ajax({
+                type: 'GET',
+                url: "https://repkam09.com/repserv/corsget/" + btoa(url),
+                async: true,
+                success: function (json) {
+                    debugger;
+                    resolve(json);
+                },
+                error: function (e) {
+                    console.log(e.message);
+                    reject(e);
+                }
+            });
+        });
+    }
+
 };
